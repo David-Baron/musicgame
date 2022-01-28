457a8d0399a4609af09d38153e17a5e002d10f98
David-Baron <davidbaron@mycodingsoft.com>
2022-01-28 10:52:16 +0100

    Typo clean code

--- src/Repository/MusicgamePlaylistRepository.php
+++ src/Repository/MusicgamePlaylistRepository.php
@@ -18,33 +18,4 @@ class MusicgamePlaylistRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, MusicgamePlaylist::class);
     }
-
-    // /**
-    //  * @return MusicgamePlaylist[] Returns an array of MusicgamePlaylist objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('m.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?MusicgamePlaylist
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/MusicgameTrackRepository.php
+++ src/Repository/MusicgameTrackRepository.php
@@ -19,32 +19,4 @@ class MusicgameTrackRepository extends ServiceEntityRepository
         parent::__construct($registry, MusicgameTrack::class);
     }
 
-    // /**
-    //  * @return MusicgameTrack[] Returns an array of MusicgameTrack objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('m.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?MusicgameTrack
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
